bc900942062bdf29080fbf8bb0d05c9ed03b459c
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-06 18:05:24 +0300

    Completed the EmailForm project implementation.

---
+++ EmailForm/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  email: string = 'Zehra';
+
+  onSubmit() {
+    console.log(this.email);
+  }
+}
